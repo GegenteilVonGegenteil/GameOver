b1f092d3b01e8a8567a4a47557c9d8e2d0c4b9b1
kajak <kaja.kitzmueller@gmail.com>
2024-07-15 23:38:43 +0200

    last

--- services/database.js
+++ services/database.js
@@ -1,20 +1,5 @@
 require('dotenv').config()
-const mysql = require('mysql2')
 const { Sequelize } = require ('sequelize')
-
-// connection config with the credentials stored in the .env file
-const config = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-// connect to the database
-config.connect(function(err){
-    if (err) throw err;
-});
 
 // create a new sequelize instance with the connection
 const sequelize = new Sequelize(
@@ -23,12 +8,27 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
         dialect: 'mysql',
+        dialectModule:require("mysql2"),
         dialectOptions: {
+            connectTimeout: 60000,
             ssl: {
                 require: true,
                 rejectUnauthorized: false,
             },
+            pool: {
+                max: 5,
+                min: 0,
+                acquire: 30000,
+                idle: 10000
+            },
         },
+        logging: (msg) => console.log(msg), // Enable detailed logging
     });
 
-module.exports = { config, sequelize };
+sequelize.authenticate().then(() => {
+    console.log('[SUCCESS] Connection has been established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to the database:', error);
+});
+
+module.exports ={sequelize};
