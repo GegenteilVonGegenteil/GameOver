f683c6b65ed24832463668d04b015d224263babc
kajak <kaja.kitzmueller@gmail.com>
2024-07-15 23:01:47 +0200

    init

--- services/database.js
+++ services/database.js
@@ -6,7 +6,7 @@ const { Sequelize } = require ('sequelize')
 const config = mysql.createConnection({
     host: process.env.DB_HOST,
     port:process.env.DB_PORT,
-    user: process.env.DB_USER,
+    user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
